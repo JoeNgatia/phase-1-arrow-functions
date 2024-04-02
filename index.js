dd1fcd257852692f27c5289b9547f6a6ebf8cf2c
joengatia <ngatiajoe2@gmail.com>
2024-04-02 11:31:58 +0300

    intial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Define a function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  }
+  
+  // Define an arrow function called square
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // Define an arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  }
+  
